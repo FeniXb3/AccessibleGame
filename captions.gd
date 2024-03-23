59839f92cd615cc1812cb54ec9b5685ab4337029
Konrad Gadzina <konrad@makegames.today>
2024-03-23 01:15:03 +0100

    Display kinda proper subtitles from SRT file

--- captions.gd
+++ captions.gd
@@ -1,147 +1,126 @@
 extends Node
 
+func prepare_track(animation : Animation, node, update_mode : Animation.UpdateMode, property = "text") -> int:
+	var track_index := animation.add_track(Animation.TYPE_VALUE)
+	var owner = node.owner
+	var nodePath = NodePath(node.owner.get_path_to(node))
+	animation.value_track_set_update_mode(track_index, update_mode)
+	animation.track_set_path(track_index, "%s:%s" % [nodePath.get_concatenated_names(), property])
+	
+	return track_index
 
-func generate_animation_WORDS(data, caption_fields):
-	var animation = Animation.new()
-	var track_index = animation.add_track(Animation.TYPE_VALUE)
-	var nodePath = NodePath(data["Label"].owner.get_path_to(data["Label"]))
-	animation.value_track_set_update_mode(track_index, Animation.UPDATE_DISCRETE)
-	animation.track_set_path(track_index, nodePath.get_concatenated_names() + ":text")
-	var last_field = caption_fields[caption_fields.size() - 1]
-	caption_fields.remove_at(caption_fields.size() - 1)
-	var written = ""
+func add_segment_word_by_word_to_track(written : String, caption, animation : Animation, track_index : int, end_override := 0.0) -> String:
+	var text := caption["text"] as String # The text being displayed.
+	var start := caption["start"] as float # The starting keyframe in seconds.
+	var end := caption["end"] as float if not end_override else end_override # The ending keyframe in seconds.
+	var words := text.split(" ") # Get every word.
+	var segment_duration := end - start
+	var word_duration = segment_duration / words.size()
+
+	var currentTime = start
+	for word in words:
+		currentTime += word_duration
+		written += word + " "
+		animation.track_insert_key(track_index, currentTime, written)
+		
+	return written
+
+func generate_animation_WORDS(data : Dictionary, caption_fields):
+	var animation := Animation.new()
+	var track_index := prepare_track(animation, data["Label"], Animation.UPDATE_DISCRETE)
+	var last_field = caption_fields.pop_back()
+	var written := ""
 	for caption in caption_fields:
-		var text = caption["text"] # The text being displayed.
-		var start = caption["start"] # The starting keyframe in seconds.
-		var end = caption["end"] # The ending keyframe in seconds.
-		if text.find(" ") != -1: # Check for whitespaces.
-			var words = text.split(" ") # Get every word.
-			var midpoint = end - start
-			var WPS = midpoint / words.size()
-			var wordFrequency = WPS
-			var currentTime = start
-			for word in words:
-				currentTime += wordFrequency
-				written += word + " "
-				animation.track_insert_key(track_index, currentTime, written)
-		else: # Return the single word.
-			written += text + " "
-			animation.track_insert_key(track_index, end, written)
-
-	var text = last_field["text"]
-	written += " "
+		written = add_segment_word_by_word_to_track(written, caption, animation, track_index)
 	
-	var duration
-	
-	if "Duration" in data: # Check whether a custom duration was provided.
-		duration = data["Duration"]
-	else:
-		duration = last_field["end"]
-
-	if text.find(" ") != -1:
-		var start = last_field["start"]
-		var end = duration
-		var words = text.split(" ") # Get every word.
-		var midpoint = end - start
-		var WPS = midpoint / words.size()
-		var wordFrequency = WPS
-		var currentTime = start
-		for word in words:
-			currentTime += wordFrequency
-			written += word + " "
-			animation.track_insert_key(track_index, currentTime, written)
-	else:
-		animation.track_insert_key(track_index, duration, written)
+	var duration : float = data.get("Duration", last_field["end"]) 
+	written = add_segment_word_by_word_to_track(written, last_field, animation, track_index, duration)
 	
 	animation.length = duration
 	
-	var animPlayer
-	
 	if "AnimationPlayer" in data and "Name" in data: # Adds the named animation to the provded animation player.
-		animPlayer = data["AnimationPlayer"]
-		animPlayer.get_animation_library("").add_animation(data["Name"], animation)
+		data["AnimationPlayer"].get_animation_library("").add_animation(data["Name"], animation)
 		return true
 	else: # Returns the animation.
 		return animation
-	
-
-
 
 func generate_animation_LETTERS(data, caption_fields):
-	var animation = Animation.new()
-	var track_index = animation.add_track(Animation.TYPE_VALUE)
-	var nodePath = NodePath(data["Label"].owner.get_path_to(data["Label"]))
+	var animation := Animation.new()
+	var track_index = prepare_track(animation, data["Label"], Animation.UPDATE_CONTINUOUS)
+	var last_field = caption_fields.pop_back()
 	var full_script = ""
-	animation.value_track_set_update_mode(track_index, Animation.UPDATE_CONTINUOUS)
-	animation.track_set_path(track_index, nodePath.get_concatenated_names() + ":text")
-	var last_field = caption_fields[caption_fields.size() - 1]
-	caption_fields.remove_at(caption_fields.size() - 1)
+	var text := ""
 	for caption in caption_fields:
-		var text = caption["text"] # The text being displayed.
+		text = caption["text"] # The text being displayed.
 		var start = caption["start"] # The starting keyframe in seconds.
 		var end = caption["end"] # The ending keyframe in seconds.
 		animation.track_insert_key(track_index, start, full_script)
 		full_script += text + " "
 		animation.track_insert_key(track_index, end, full_script)
 	
-	var text = last_field["text"]
+	text = last_field["text"]
 
 	animation.track_insert_key(track_index, last_field["start"], full_script)
 	full_script += last_field["text"]
-	
-	var duration
-	
-	if "Duration" in data: # Check whether a custom duration was provided.
-		duration = data["Duration"]
-	else:
-		duration = last_field["end"]
+		
+	var duration : float = data.get("Duration", last_field["end"]) 
 	
 	animation.track_insert_key(track_index, duration, full_script)
 	
 	animation.length = duration
 	
-	var animPlayer
-	
 	if "AnimationPlayer" in data and "Name" in data: # Adds the named animation to the provded animation player.
-		animPlayer = data["AnimationPlayer"]
-		animPlayer.get_animation_library("").add_animation(data["Name"], animation)
+		data["AnimationPlayer"].get_animation_library("").add_animation(data["Name"], animation)
 		return true
 	else: # Returns the animation.
 		return animation
 	
+func generate_animation_subtitles(data, caption_fields):
+	var animation := Animation.new()
+	var track_index = prepare_track(animation, data["Label"], Animation.UPDATE_DISCRETE)
+	var container_track_index = prepare_track(animation, data["Container"], Animation.UPDATE_DISCRETE, "visible")
+	print((data["AnimationPlayer"] as AnimationPlayer).root_node)
+	for caption in caption_fields:
+		var text = caption["text"]
+		var start = caption["start"]
+		var end = caption["end"]
+		animation.track_insert_key(container_track_index, start, true)
+		animation.track_insert_key(track_index, start, text)
+		animation.track_insert_key(track_index, end, "")
+		animation.track_insert_key(container_track_index, end, false)
+	
+	var duration : float = caption_fields.back()["end"]# - caption_fields.front()["start"]
+	animation.length = duration
+	
+	if "AnimationPlayer" in data and "Name" in data: # Adds the named animation to the provded animation player.
+		data["AnimationPlayer"].get_animation_library("").add_animation(data["Name"], animation)
+	
+	return animation
+
+func parse_subtitles(content : String) -> Array:
+	var caption_fields := []	
+	var content_regex := RegEx.new()
+	content_regex.compile(r'\d+[\r\n]((?<start>\d+:\d+:\d+,\d+) --> (?<end>\d+:\d+:\d+,\d+))[\r\n](?<text>(.+\r?\n)+(?=(\r?\n)?))')
+	for result in content_regex.search_all(content):
+		caption_fields.append({
+			"text": result.get_string("text").strip_edges(), 
+			"start": timestamp_to_seconds(result.get_string("start")), 
+			"end": timestamp_to_seconds(result.get_string("end"))
+		})
+	return caption_fields
 
 func create(data: Dictionary): # Creates and returns a label animation with the captions provided.
+	var caption_fields := []
 	
-	var caption_fields = []
-	var messy_values = []
-	
-	var raw_file
-	
-	if data["TextPath"].begins_with("res://") or data["TextPath"].begins_with("user://") or data["TextPath"].begins_with("C:/"): # File.
-		raw_file = FileAccess.open(data["TextPath"], FileAccess.READ)
-		while not raw_file.eof_reached():
-			var line = raw_file.get_line()
-			if line.find("-->") != -1: # Check if the line is a timestamp.
-				var timestamps = get_timestamp_values(line) # Returns an array containing the start and end of the following line in seconds.
-				messy_values.append(timestamps[0])
-				messy_values.append(timestamps[1])
-			elif messy_values.size() == 2: # If we have 2 stamps then this line must be the transcribed text.
-				var dict = {"text": line, "start": messy_values[0], "end": messy_values[1]} # The dictionary used for creating keyframes.
-				caption_fields.append(dict)
-				messy_values.clear() # Clear the array for the next transcription.
+	var text_path : String = data["TextPath"]
+	if text_path.is_absolute_path():
+		var raw_file := FileAccess.open(text_path, FileAccess.READ)
+		var content := raw_file.get_as_text()
+		caption_fields = parse_subtitles(content)
 		raw_file.close()
-	else: # String.
-		var everyLine = data["TextPath"].split("\n")
-		for line in everyLine:
-			if line.find("-->") != -1: # Check if the line is a timestamp.
-				var timestamps = get_timestamp_values(line) # Returns an array containing the start and end of the following line in seconds.
-				messy_values.append(timestamps[0])
-				messy_values.append(timestamps[1])
-			elif messy_values.size() == 2: # If we have 2 stamps then this line must be the transcribed text.
-				var dict = {"text": line, "start": messy_values[0], "end": messy_values[1]} # The dictionary used for creating keyframes.
-				caption_fields.append(dict)
-				messy_values.clear() # Clear the array for the next transcription.
-	
+	else:
+		caption_fields = parse_subtitles(text_path)
+		
 	if "TimeOnly" in data and data["TimeOnly"]: # Returns caption fields without animating them.
 		return caption_fields
 	
@@ -149,6 +128,8 @@ func create(data: Dictionary): # Creates and returns a label animation with the 
 		if data["Style"].to_lower() == "word":
 			var outcome = generate_animation_WORDS(data, caption_fields)
 			return outcome
+		elif  data["Style"].to_lower() == "subtitles":
+			return generate_animation_subtitles(data, caption_fields)
 		else:
 			var outcome = generate_animation_LETTERS(data, caption_fields)
 			return outcome
@@ -156,25 +137,9 @@ func create(data: Dictionary): # Creates and returns a label animation with the 
 		var outcome = generate_animation_LETTERS(data, caption_fields)
 		return outcome
 
-
-func get_timestamp_values(raw_time_stamp): # Get start and end keyframe seconds.  (Fun fact: Chat-GPT wrote this function)
-	# Get the timestamp values
-	
-	var stamps = raw_time_stamp.split(" --> ")
-	
-	var start_timestamp = stamps[0]
-	var end_timestamp = stamps[1]
-
-	# Split the timestamp into an array
-	var start_arr = start_timestamp.split(":")
-	var end_arr = end_timestamp.split(":")
-
-	# Calculate the total number of seconds
-	var start_seconds = float(start_arr[0]) * 3600 + float(start_arr[1]) * 60 + float(start_arr[2].split(",")[0]) + float(start_arr[2].split(",")[1]) / 1000.0
-	var end_seconds = float(end_arr[0]) * 3600 + float(end_arr[1]) * 60 + float(end_arr[2].split(",")[0]) + float(end_arr[2].split(",")[1]) / 1000.0
-	
-	return [start_seconds, end_seconds]
-
+func timestamp_to_seconds(timestamp : String) -> float:
+	var segments = timestamp.split(":")
+	return float(segments[0]) * 3600 + float(segments[1]) * 60 + float(segments[2].split(",")[0]) + float(segments[2].split(",")[1]) / 1000.0
 
 func get_complete_template():
 	var template = {"TextPath": "PATH TO .TXT FILE (REQUIRED)", # Required
--- subtitles_layer.gd
+++ subtitles_layer.gd
@@ -1,9 +1,17 @@
 extends CanvasLayer
 @onready var animation_player = %AnimationPlayer
-@onready var tmp = $CanvasLayer/Panel/Tmp
-@onready var subtitle = %Subtitle
+@onready var subtitle : RichTextLabel = %Subtitle
+@onready var external_margin_container = %ExternalMarginContainer
 
 func _ready():
-	var data = {"TextPath": "res://text_file_format_example.txt", "Label": subtitle, "AnimationPlayer": animation_player, "Name": "display_subtitles", "Style": "word"} # Settings which will be passed as an argument.
+	var data = {
+		"TextPath": "res://star_wars_example.txt", 
+		"Label": subtitle,
+		"AnimationPlayer": animation_player, 
+		"Name": "display_subtitles", 
+		"Style": "subtitles",
+		"Container": external_margin_container,
+		# "Duration": 15
+	} # Settings which will be passed as an argument.
 	Captions.create(data)
 	animation_player.play("display_subtitles")
