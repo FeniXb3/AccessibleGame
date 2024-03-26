ae15dbf200e70872f629c5fe0826e56b96e6a173
Konrad Gadzina <konrad@makegames.today>
2024-03-27 00:56:31 +0100

    Modify subtitle font size with keyboard arrows

--- subtitles_layer.gd
+++ subtitles_layer.gd
@@ -2,9 +2,12 @@ extends CanvasLayer
 @onready var animation_player = %AnimationPlayer
 @onready var subtitle : RichTextLabel = %Subtitle
 @onready var external_margin_container = %ExternalMarginContainer
+@export var theme : Theme
 @export var speech_player : AudioStreamPlayer
 
 func _ready():
+	external_margin_container.theme = theme
+	
 	var data = {
 		"TextPath": "res://star_wars_example.txt", 
 		"Label": subtitle,
@@ -17,3 +20,13 @@ func _ready():
 	animation_player.play("display_subtitles")
 	if speech_player:
 		speech_player.play()
+
+func _process(delta):
+	if Input.is_action_just_pressed("ui_up"):
+		var current_font_size := theme.default_font_size
+		print(current_font_size)
+		theme.default_font_size += 10
+	elif Input.is_action_just_pressed("ui_down"):
+		var current_font_size := theme.default_font_size
+		print(current_font_size)
+		theme.default_font_size -= 10
