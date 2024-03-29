764f4b0461140e02f1fae0de3204bbdf6d654e15
Konrad Gadzina <konrad@makegames.today>
2024-03-29 15:38:27 +0100

    Customize color picker

--- color_option.gd
+++ color_option.gd
@@ -17,3 +17,15 @@ func _on_color_variable_value_changed(new_value : Color):
 
 func _on_color_picker_button_color_changed(color):
 	color_variable.value = color
+
+
+func _on_color_picker_button_picker_created():
+	var picker : ColorPicker = color_picker_button.get_picker()
+	picker.color_mode = ColorPicker.MODE_HSV
+	picker.picker_shape = ColorPicker.SHAPE_HSV_WHEEL
+	picker.sampler_visible = false
+	picker.color_modes_visible = false
+	picker.sliders_visible = false
+	picker.hex_visible = false
+	picker.presets_visible = false
+	picker.can_add_swatches = false
