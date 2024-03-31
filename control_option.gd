b6a39c6408e7bb6da653a5c0646e80f8dd97a3c9
Konrad Gadzina <konrad@makegames.today>
2024-03-31 09:00:35 +0200

    Add simple controls setting

---
+++ control_option.gd
@@ -0,0 +1,38 @@
+extends HBoxContainer
+class_name ControlOption
+
+@export var action : String
+@export var events : Array[InputEvent]
+@export var variable : ColorVariable
+
+@onready var label = %Label
+@onready var popup_panel = $PopupPanel
+@onready var button = %Button
+
+var consume_input : bool
+
+
+
+func _ready():
+	label.text = action
+	button.text = events[0].as_text()
+	button.icon = null
+
+func _on_button_pressed():
+	button.icon = null
+	button.text = "..."
+	consume_input = true
+	
+func _input(event):
+	if not consume_input:
+		return
+
+	get_viewport().set_input_as_handled()
+	consume_input = false
+
+	if event.is_action_pressed("ui_cancel"):
+		return
+	
+	InputMap.action_erase_event(action, events[0])
+	InputMap.action_add_event(action, event)
+	button.text = event.as_text()
---
+++ controls_options.gd
@@ -0,0 +1,27 @@
+extends PanelContainer
+
+@export var single_option_scene : PackedScene
+
+@onready var controls_parent = %ControlsParent
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	var all_actions := InputMap.get_actions()
+	
+	for action in all_actions:
+		if not action.begins_with("ui_"):
+			var events := InputMap.action_get_events(action)
+			print("{action} : {events}".format({"action":action, "events":events}))
+			if events.is_empty():
+				continue
+				
+			var new_control_option := single_option_scene.instantiate() as ControlOption
+			new_control_option.action = action
+			new_control_option.events = events
+			
+			controls_parent.add_child(new_control_option)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
