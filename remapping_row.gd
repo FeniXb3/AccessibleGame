1408e45a6a17eb9f506c71596e185ee7b2c3aba4
Konrad Gadzina <konrad@makegames.today>
2024-04-06 16:03:05 +0200

    Make remap row setup without creating subnodes with code

--- remapping_row.gd
+++ remapping_row.gd
@@ -6,20 +6,28 @@ signal remove_button_pressed(row)
 @export var action : StringName
 @export var action_remap_button_scene : PackedScene
 
-#@onready var action_remap_button : ActionRemapButton = %ActionRemapButton
-var action_remap_button : ActionRemapButton
+@onready var action_remap_button : ActionRemapButton = %ActionRemapButton
+#var action_remap_button : ActionRemapButton
 @onready var remove_action_event = %RemoveActionEvent
+@export var remap_event_type: ActionRemapButton.RemapEventType
+@export var action_event : InputEvent
 
 func setup(action_to_remap : String, remap_type: ActionRemapButton.RemapEventType, current_event : InputEvent):
 	action = action_to_remap
-	action_remap_button = action_remap_button_scene.instantiate()
-	action_remap_button.setup(action_to_remap, remap_type, current_event)
-	add_child(action_remap_button)
+	remap_event_type = remap_type
+	action_event = current_event
+	#action_remap_button = action_remap_button_scene.instantiate()
+	#action_remap_button.setup(action_to_remap, remap_type, current_event)
+	#add_child(action_remap_button)
 	
-	var remove_button := Button.new()
-	remove_button.text = "Remove"
-	remove_button.pressed.connect(_on_remove_action_event_pressed)
-	add_child(remove_button)
+	#var remove_button := Button.new()
+	#remove_button.text = "Remove"
+	#remove_button.pressed.connect(_on_remove_action_event_pressed)
+	#add_child(remove_button)
+	
+func _ready():
+	action_remap_button.setup(action, remap_event_type, action_event)
+
 	
 func _on_remove_action_event_pressed():
 	InputMap.action_erase_event(action, action_remap_button.action_event)
