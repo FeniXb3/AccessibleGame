bf83f795a1259ada5ed8866640420eff7740c9c7
Konrad Gadzina <konrad@makegames.today>
2024-04-15 11:26:39 +0200

    Experiment with camera rotation in TPP view

--- player_3d.gd
+++ player_3d.gd
@@ -1,5 +1,13 @@
 extends CharacterBody3D
 
+@export var min_angles := Vector2(0, -45)
+@export var max_angles := Vector2(360, 90)
+
+@export var current_angles := Vector2(0, 0)
+
+@onready var horizontal_pivot: Node3D = %HorizontalPivot
+@onready var vertical_pivot: Node3D = %VerticalPivot
+
 
 const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
@@ -24,5 +32,26 @@ func _physics_process(delta: float) -> void:
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.z = move_toward(velocity.z, 0, SPEED)
+
+	# Based on https://stackoverflow.com/a/77147323/1816426
+	var camera_rotation := InputEnhancer.get_vector("camera_rotate_left", "camera_rotate_right", "camera_rotate_up", "camera_rotate_down") * delta
+	current_angles.x = wrapf(current_angles.x, -PI, PI) + camera_rotation.x
+	current_angles.y = wrapf(current_angles.y, -PI, PI) + camera_rotation.y
+
+	current_angles.x = clampf(
+		wrapf(current_angles.x, -PI, PI),
+		deg_to_rad(min_angles.x),
+		deg_to_rad(max_angles.x)
+	)
+
+	current_angles.y = clampf(
+		wrapf(current_angles.y, -PI, PI),
+		deg_to_rad(min_angles.y),
+		deg_to_rad(max_angles.y)
+	)
+
+
+	horizontal_pivot.basis = Basis(Vector3.UP, current_angles.x)
+	vertical_pivot.basis = Basis(Vector3.RIGHT, current_angles.y)
 
 	move_and_slide()
